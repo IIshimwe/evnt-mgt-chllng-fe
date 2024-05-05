45fb071fadd0b46ece07b01a7d319844e7d0236f
IIshimwe <ixacson@gmail.com>
2024-05-05 23:04:28 +0200

    Add images to card template with an improved description

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,36 @@ function App() {
     <div style={{ display: "flex", gap: "2rem" }}>
       <EventCard
         title="Event title"
-        description="Event description"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste perferendis
+        tempore numquam ab doloribus, non voluptatem ipsam odit fugiat sapiente..."
         date="2024-05-05"
         location="Serena Hotel"
-        image={"/img/event.jpg"}
+        image={
+          "https://images.pexels.com/photos/15475219/pexels-photo-15475219/free-photo-of-hill-in-black-and-white.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }
+        link={eventLink}
+      />
+
+      <EventCard
+        title="Event title"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste perferendis
+        tempore numquam ab doloribus, non voluptatem ipsam odit fugiat sapiente..."
+        date="2024-05-05"
+        location="Serena Hotel"
+        image={
+          "https://images.pexels.com/photos/19882695/pexels-photo-19882695/free-photo-of-concrete-breakers-on-the-sea-coast.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }
         link={eventLink}
       />
       <EventCard
         title="Event title"
-        description="Event description"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste perferendis
+        tempore numquam ab doloribus, non voluptatem ipsam odit fugiat sapiente..."
         date="2024-05-05"
         location="Serena Hotel"
-        image={"/img/event.jpg"}
-        link={eventLink}
-      />
-      <EventCard
-        title="Event title"
-        description="Event description"
-        date="2024-05-05"
-        location="Serena Hotel"
-        image={"/img/event.jpg"}
+        image={
+          "https://images.pexels.com/photos/22880134/pexels-photo-22880134/free-photo-of-a-woman-in-a-hijab-standing-in-a-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        }
         link={eventLink}
       />
     </div>
